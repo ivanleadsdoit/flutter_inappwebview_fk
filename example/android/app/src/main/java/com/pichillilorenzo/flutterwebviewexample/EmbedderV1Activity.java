fa4346b046e1a8a2418d20e48c293d57d2432ca8
ivan.radchenko <iv.r@dcjudgement.com>
2023-10-30 14:47:15 +0100

    init commit

---
+++ example/android/app/src/main/java/com/pichillilorenzo/flutterwebviewexample/EmbedderV1Activity.java
@@ -0,0 +1,14 @@
+package example.android.app.src.main.java.com.pichillilorenzo.flutterwebviewexample;
+
+import android.os.Bundle;
+import com.pichillilorenzo.flutter_inappwebview.InAppWebViewFlutterPlugin;
+
+@SuppressWarnings("deprecation")
+public class EmbedderV1Activity extends io.flutter.app.FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    InAppWebViewFlutterPlugin.registerWith(
+            registrarFor("com.pichillilorenzo.flutter_inappwebview.InAppWebViewFlutterPlugin"));
+  }
+}
---
+++ example/android/app/src/main/java/com/pichillilorenzo/flutterwebviewexample/MainActivity.java
@@ -0,0 +1,7 @@
+package example.android.app.src.main.java.com.pichillilorenzo.flutterwebviewexample;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+
+}
